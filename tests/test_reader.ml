c129097ee8b12014faaaedff52164f7d3e56a1ac
Etienne MARAIS <etienne@marais.green>
2019-07-04 13:18:42 +0200

    monad: change reader tests
    Modify reader tests to accept the choice monad

--- tests/test_reader.ml
+++ tests/test_reader.ml
@@ -6,27 +6,29 @@
 open Alcotest
 open Core
 
+
 (* File original content *)
 let test_content = [
   "Luke, I'm your father" ;
   "42 is the answer" ;
   "I never know if Sally tried to protect me" ;
-  "For the livings!" ;
+  "For the livings!";
   ""
 ]
 
 let wrong_file _ = 
-   (check (option (list string)))
-      "Check wrong file" None (Reader.read_file "Wonderland")
-
+  let file = Reader.read_file "wonderland" in
+  match file with
+  | Error a -> (check (string)) "Compare errors" "[Error] Can't open file" a
+  | _ -> raise Test_error
 
 let right_file _ = 
   let file = Reader.read_file "res/Read_content.test" in
   match file with
-  | None ->  raise Test_error
-  | Some content -> 
+  | Error _ ->  raise Test_error
+  | Ok  content -> 
       (check (list string)) "Compare strings" content test_content
-      
+
 
 
 
