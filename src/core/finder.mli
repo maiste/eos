234ce0bcce8a180801ef12e90859174ba21f154b
Etienne MARAIS <etienne@marais.green>
2019-07-07 16:36:25 +0200

    monad: update conf and finder

--- src/core/finder.mli
+++ src/core/finder.mli
@@ -3,9 +3,11 @@
  * DURAND - MARAIS © 2019
  *)
 
+open Monad
+
 (*
    Tail-recursive search of all files corresponded to the [target] list of regex. 
    [target] list of regex to match files.
    It returns list of all targeted files.
 *)
-val get_all_files : string list -> string list
+val get_files : string list -> string list choice
