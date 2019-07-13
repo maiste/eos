abeafaecc072c2a586b6b04521c4e306b84455af
Xavitator <xavier75013@gmail.com>
2019-07-13 19:27:23 +0200

    ezjsonm : convert Yojson to ezjsonm

--- src/core/conf.mli
+++ src/core/conf.mli
@@ -9,10 +9,10 @@ open Monad
 val conf_file : string
 
 (* init [json] variable *)
-val init_json : string -> Yojson.Basic.t choice
+val init_json : string -> Ezjsonm.value choice
 
 (* Get string content corresponded to [name] field *)
-val get_name : Yojson.Basic.t -> string choice
+val get_name : Ezjsonm.value -> string choice
 
 (* Get all regex corresponded to the [files] content *)
-val get_file_regex : Yojson.Basic.t -> string list choice
+val get_file_regex : Ezjsonm.value -> string list choice
