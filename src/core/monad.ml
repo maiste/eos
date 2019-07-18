b9fd011358d2ead1995d50e8cbba6dc21667a7e1
Etienne MARAIS <etienne@marais.green>
2019-07-18 18:42:14 +0200

    opt: add option bind

--- src/core/monad.ml
+++ src/core/monad.ml
@@ -5,7 +5,20 @@
 
 type 'a choice = ('a, string) result
 
+(* Bind for 'a choice *)
 let (>>=) obj f =
   match obj with
   | Ok a -> f a
   | Error b -> Error b
+
+(* Bind for option *)
+let (>==) obj f = 
+  match obj with
+  | Some a -> f a 
+  | None -> None
+
+(* From monad option to monad choice *)
+let opt_to_choice str_error obj =
+  match obj with
+  | Some a -> Ok a
+  | None -> Error str_error
