4d0e4ff52334e0e05c41860c0e94d9a7032bc48f
Etienne MARAIS <etienne@marais.green>
2019-07-18 18:42:14 +0200

    opt: add option bind

--- src/core/monad.mli
+++ src/core/monad.mli
@@ -5,5 +5,11 @@
 
 type 'a choice = ('a, string) result
 
-(** Bind function *)
+(** Bind function for 'a choice *)
 val (>>=) : 'a choice -> ('a -> 'b choice) -> 'b choice
+
+(** Bind function for option *)
+val (>==) : 'a option -> ('a -> 'b option) -> 'b option
+
+(** Function to move from option to choice *)
+val opt_to_choice : string -> 'a option -> 'a choice
