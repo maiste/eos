e58bccd9de6dd866affdebdd1777948c0cd4da91
Etienne MARAIS <etienne@marais.green>
2019-07-23 13:18:41 +0200

    comment: add comments tests

--- tests/main_test.ml
+++ tests/main_test.ml
@@ -5,13 +5,14 @@
 
 
 let test_core = [
-  "Hello", Test_hello.suite         ;
-  "Reader", Test_reader.suite       ;
-  "Finder", Test_finder.suite       ;
-  "Conf", Test_conf.suite           ;
-  "FString", Test_fstring.suite     ;
-  "Formatter", Test_formatter.suite ;
+  "Hello", Test_hello.suite           ;
+  "Reader", Test_reader.suite         ;
+  "Finder", Test_finder.suite         ;
+  "Conf", Test_conf.suite             ;
+  "FString", Test_fstring.suite       ;
+  "Formatter", Test_formatter.suite   ;
   "Comparator", Test_comparator.suite ;
+  "Comment", Test_comment.suite       ;
 ]
 
 
