9121cbe8852dabb55775fa8a18a054b15ac62504
Etienne MARAIS <etienne@maiste.fr>
2021-07-03 22:45:58 +0200

    docs: update documentation for mli
    * It adds license header to mli files.
    * It formats with ocamlformat the mli files.

--- src/core/FString.mli
+++ src/core/FString.mli
@@ -1,20 +1,41 @@
-(**
- * Interface for the module 
- * FString 
- * DURAND-MARAIS © 2019
-*)
+(**********************************************************************************)
+(* MIT License                                                                    *)
+(*                                                                                *)
+(* Copyright (c) 2019 Étienne Marais <etienne@maiste.fr>                          *)
+(* Copyright (c) 2019 Xavier Durand <xavier75013@gmail.com>                       *)
+(*                                                                                *)
+(* Permission is hereby granted, free of charge, to any person obtaining a copy   *)
+(* of this software and associated documentation files (the "Software"), to deal  *)
+(* in the Software without restriction, including without limitation the rights   *)
+(* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *)
+(* copies of the Software, and to permit persons to whom the Software is          *)
+(* furnished to do so, subject to the following conditions:                       *)
+(*                                                                                *)
+(* The above copyright notice and this permission notice shall be included in all *)
+(* copies or substantial portions of the Software.                                *)
+(*                                                                                *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *)
+(* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *)
+(* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *)
+(* SOFTWARE.                                                                      *)
+(**********************************************************************************)
 
 open Monad
 
-(** Type corresponding to a position *)
-type position = 
-  | Center (** Center align (add space before and after the string if needed) *)
-  | Left (** Left align (add after the string if needed) *)
-  | Right (** Right align (add before the string if needed) *)
+(** Type corresponding to a position. *)
+type position =
+  | Center
+      (** Center align (add space before and after the string if needed) *)
+  | Left  (** Left align (add after the string if needed) *)
+  | Right  (** Right align (add before the string if needed) *)
 
-(** Get string of a [position] *)
+(** Gets string of a [position]. *)
 val position_str : position -> string
 
-(** Main function to format string where [pos] is the position of the string,
-    [size] the size of the string and [content] the content of the string *)
+(** Main function to format string where [pos] is the position of the
+    string, [size] the size of the string and [content] the content of
+    the string. *)
 val format_string : string option -> int option -> string -> string choice
